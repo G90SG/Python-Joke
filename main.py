3e3669edcd48c994b9469ddbbb8cae61765a8a54
Gemma Dingwall <gemma9261@hotmail.com>
2020-09-28 13:44:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+joke = int(input("If you would like to hear a joke, enter a number between 1-10?"))
+if (joke == 1):
+  ans1 = input("What does a butcher do after travelling somewhere over the rainbow?")
+  print ("Weigh a Pie.")
+elif (joke == 2):
+  ans2 = input("What's the difference between a well dressed man on a unicycle and a poorly dressed man on a unicycle?")
+  print ("Attire.")
+elif (joke == 3):
+  ans3 = input ("Someone stole my mood ring")
+  print ("I don't know how I feel about it.")
+elif (joke == 4):
+  ans4 = input ("What does a biologist wear on a first date?")
+  print ("Designer Genes.")
+elif (joke == 5):
+  ans5 = input ("What did one eye say to the other?")
+  print ("Between you and me, something smells.")
+elif (joke == 6):#
+  ans6 = input ("I really like boiling fruit for hours")
+  print ("It's my jam.")
+elif (joke == 7):
+  ans7 = input ("What's a vampire's favourite fruit?")
+  print ("A Neck-tarine.")
+elif (joke == 8):
+  ans8 = input ("Why did the skeleton cross the road?")
+  print ("To get to the bodyshop.")
+elif (joke == 9):
+  ans9 = input ("Want to hear a poop joke?")
+  print ("Nevermind, it stinks.")
+elif (joke == 10):
+  ans10 = input ("What do you call a magic dog?")
+  print ("Labra-cadabra-dor.")
+else: 
+  print("You didn't choose the right option, i'm not feeling funny anymore.")
